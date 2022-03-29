41090a881e53af5e942f6b26b275fccc12e6d8b9
Mindohyeon <min50875@gmail.com>
2022-03-29 21:53:05 +0900

    NavigationLink 로 연결하고 BackButton 지우기

--- iOS/Sources/View/StartView.swift
+++ iOS/Sources/View/StartView.swift
@@ -9,41 +9,50 @@
 import SwiftUI
 
 struct StartView: View {
+    @State var isAct : Bool = false
     var body: some View {
-        
-        VStack(alignment: .center, spacing: 66) {
-            Image("WhoAreYou-text")
+        NavigationView {
             
-            Image("WhoAreYou-logo")
-            
-            Text("Who Are You 는 익명의 상대와\n 편하게 대화할 수 있는\n 익명 채팅 서비스 입니다.")
-                .frame(maxWidth: 375, maxHeight: 96)
-                .font(.title2)
-            //font 를 가운데로 정렬
-                .multilineTextAlignment(.center)
-            
-            Button {
+            VStack(alignment: .center, spacing: 66) {
+                Image("WhoAreYou-text")
                 
-            } label: {
-                ZStack {
+                Image("WhoAreYou-logo")
+                
+                Text("Who Are You 는 익명의 상대와\n 편하게 대화할 수 있는\n 익명 채팅 서비스 입니다.")
+                    .frame(maxWidth: 375, maxHeight: 96)
+                    .font(.title2)
+                //font 를 가운데로 정렬
+                    .multilineTextAlignment(.center)
+                
+                Button {
                     
-                    RoundedRectangle(cornerRadius: 10)
-                        .frame(maxWidth: 227, maxHeight: 40)
-                        .foregroundColor(.init(hex: "8870FE"))
+                } label: {
+                    ZStack {
+                        
+                        
+                        RoundedRectangle(cornerRadius: 10)
+                            .frame(maxWidth: 227, maxHeight: 40)
+                            .foregroundColor(.init(hex: "8870FE"))
+                        
+                        //back 버튼을 숨기기 위해 원하는 뷰 바로 뒤에 코드 추가
+                        NavigationLink( destination: WhatsYourNameView() .navigationBarBackButtonHidden(true)) {
+                            
+                            Text("시작하기")
+                                .frame(maxWidth: 67, maxHeight: 26)
+                                .foregroundColor(.white)
+                        }
+                        
+                    }
                     
-                    Text("시작하기")
-                        .frame(maxWidth: 67, maxHeight: 26)
-                        .foregroundColor(.white)
                 }
-                
             }
         }
     }
 }
-        
-        
-        struct StartView_Previews: PreviewProvider {
-            static var previews: some View {
-                StartView()
-            }
-        }
+
+
+struct StartView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartView()
+    }
+}
