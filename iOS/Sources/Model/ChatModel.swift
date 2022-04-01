e57bf9ff6da732e6793db9edbfcafcece7cd47ab
Mindohyeon <min50875@gmail.com>
2022-04-01 11:33:55 +0900

    [Add]ChatModel

--- iOS/Sources/Model/ChatModel.swift
+++ iOS/Sources/Model/ChatModel.swift
@@ -8,42 +8,10 @@
 
 import SwiftUI
 
-struct ChatModel {
-    let person : Person
-    var message : [Message]
+struct ChatModel: Codable {
+    let name: String
+    let message: String
     
 }
 
-struct Person : Codable{
-    let name : String
-}
 
-struct Message : Codable {
-    
-    enum MessageType : Codable {
-        case Sent, Received
-    }
-    
-    let date : Date
-    let text : String
-    let type : MessageType
-    
-    init(text : String,type : MessageType, date : Date) {
-        self.text = text
-        self.type = type
-        self.date = date
-    }
-
-}
-
-extension ChatModel {
-    static let sampleChat = [
-        ChatModel(person: Person(name: "Dohyeon"), message: [
-            Message(text: "Hi", type: .Sent, date: Date(timeIntervalSince1970: -86400 * 3)),
-            Message(text: "Hello~~~", type: .Received, date: Date(timeIntervalSince1970: -86400 * 2)),
-            Message(text: "Hello~~@@", type: .Received, date: Date(timeIntervalSince1970: -86400 * 2)),
-            Message(text: "How are you doing know??", type: .Sent, date: Date(timeIntervalSince1970: -86400 * 1)),
-            
-        ])
-    ]
-}
