9c5dfeeb2335daf4164493cc570f829add4c5aa9
Mindohyeon <min50875@gmail.com>
2022-04-02 22:57:36 +0900

    [Add] SocketViewModel

---
+++ iOS/Sources/ViewModel/SocketViewModel.swift
@@ -0,0 +1,50 @@
+//
+//  Socket.swift
+//  WhoAreYou
+//
+//  Created by 민도현 on 2022/04/01.
+//  Copyright © 2022 baegteun. All rights reserved.
+//
+
+import Foundation
+import SocketIO
+
+//
+//let manager = SocketManager(socketURL: URL(string: "http://10.53.68.89:8080/")!,
+//                            config:  [.log(false) , .compress, .forceWebsockets(true)])
+
+class SocketViewModel : ObservableObject {
+    
+    @Published var message : String = ""
+    
+    let manager = SocketManager(socketURL: URL(string: "http://10.120.74.37:8080/")!, config: [.log(false), .compress])
+    var socket: SocketIOClient? = nil
+    
+    init() {
+        setUpSocket()
+        
+        //설정한 주소와 포트로 소켓 연결 시도
+        socket?.connect()
+    }
+    
+    func setUpSocket() {
+        self.socket = manager.defaultSocket
+    }
+    
+    func setUpSocketEvents() {
+        socket?.on(clientEvent: .connect) { data, ack in
+            print("Connected!")
+        }
+        
+        socket?.emit("updateMessage", message )
+        
+        //이름이 updateMessage 로 emit 된 이벤트 수신
+        socket?.on("updateMessage") { (data, ack) in
+            guard let dataInfo = data.first else { return }
+            
+            
+            
+        }
+    }
+}
+
