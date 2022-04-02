93c6b6a261574b6e5b0a9454e939588dc8632747
Mindohyeon <min50875@gmail.com>
2022-04-02 22:58:48 +0900

    정리

--- iOS/Sources/App/TargetApp.swift
+++ iOS/Sources/App/TargetApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TargetApp: App {
     var body: some Scene {
         WindowGroup {
-            ChatView(chat: ChatModel.sampleChat[0])
+            ChatView()
                 
         }
     }
