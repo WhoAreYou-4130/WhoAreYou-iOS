963250d0e914156eb60497929954d2103c047c3e
Mindohyeon <min50875@gmail.com>
2022-04-04 13:03:09 +0900

    [Error] 설정한 주소와 포트로 소켓 연결 실패

--- iOS/Sources/ViewModel/SocketViewModel.swift
+++ iOS/Sources/ViewModel/SocketViewModel.swift
@@ -16,36 +16,44 @@ import SocketIO
 class SocketViewModel : ObservableObject {
     
     @Published var message : String = ""
+    @Published var yourName : String = ""
     
-    let manager = SocketManager(socketURL: URL(string: "http://10.120.74.37:8080/")!, config: [.log(false), .compress])
-    var socket: SocketIOClient? = nil
+    let manager = SocketManager(socketURL: URL(string: "http://10.120.74.37:8080")!, config: [.log(false), .compress, .forceWebsockets(true), .forcePolling(true)])
+    var socket: SocketIOClient!
     
     init() {
         setUpSocket()
-        
+        setUpSocketEvents()
         //설정한 주소와 포트로 소켓 연결 시도
-        socket?.connect()
+        socket!.connect()
+        print("socket.status = \(socket.status)")
     }
     
     func setUpSocket() {
         self.socket = manager.defaultSocket
     }
     
+//    func getSocket() -> SocketIOClient {
+//        return socket
+//    }
+    
     func setUpSocketEvents() {
-        socket?.on(clientEvent: .connect) { data, ack in
-            print("Connected!")
+        socket.on(clientEvent: .connect) { data, ack in
+            print("Connected!!!!!")
         }
         
         //updateMessage 라는 이벤트 이름으로 message 데이터를 송신
-        socket?.emit("updateMessage", message )
+        socket.emit("newConnect", yourName)
         
         //이름이 updateMessage 로 emit 된 이벤트 수신
-        socket?.on("updateMessage") { (data, ack) in
+        socket.on("updateMessage") { (data, ack) in
             guard let dataInfo = data.first else { return }
             
             
             
         }
+        
+        print(yourName)
     }
 }
 
