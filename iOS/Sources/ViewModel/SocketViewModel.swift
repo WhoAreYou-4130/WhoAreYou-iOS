5d5a07e5d420172a9a75a9ec28d2ed961eb0f2cd
Mindohyeon <min50875@gmail.com>
2022-04-04 15:41:20 +0900

    [Error] 설정한 주소로 소켓 연결 실패..

--- iOS/Sources/ViewModel/SocketViewModel.swift
+++ iOS/Sources/ViewModel/SocketViewModel.swift
@@ -9,51 +9,48 @@
 import Foundation
 import SocketIO
 
-//
-//let manager = SocketManager(socketURL: URL(string: "http://10.53.68.89:8080/")!,
-//                            config:  [.log(false) , .compress, .forceWebsockets(true)])
 
 class SocketViewModel : ObservableObject {
     
     @Published var message : String = ""
     @Published var yourName : String = ""
     
-    let manager = SocketManager(socketURL: URL(string: "http://10.120.74.37:8080")!, config: [.log(false), .compress, .forceWebsockets(true), .forcePolling(true)])
+    let manager : SocketManager
     var socket: SocketIOClient!
     
     init() {
-        setUpSocket()
+        manager = SocketManager(socketURL: URL(string: "http://10.120.74.37:8080")!, config: [.log(false), .compress])
+        
+        self.socket = manager.defaultSocket
         setUpSocketEvents()
+        
+        
         //설정한 주소와 포트로 소켓 연결 시도
-        socket!.connect()
+
+        socket.connect()
+        
         print("socket.status = \(socket.status)")
     }
     
-    func setUpSocket() {
-        self.socket = manager.defaultSocket
-    }
-    
+
 //    func getSocket() -> SocketIOClient {
 //        return socket
 //    }
     
     func setUpSocketEvents() {
+
         socket.on(clientEvent: .connect) { data, ack in
             print("Connected!!!!!")
         }
-        
         //updateMessage 라는 이벤트 이름으로 message 데이터를 송신
         socket.emit("newConnect", yourName)
         
         //이름이 updateMessage 로 emit 된 이벤트 수신
-        socket.on("updateMessage") { (data, ack) in
-            guard let dataInfo = data.first else { return }
-            
-            
-            
-        }
-        
-        print(yourName)
+//        socket.on("updateMessage") { (data, ack) in
+//            guard let dataInfo = data.first else { return }
+//
+//
+//        }
     }
 }
 
